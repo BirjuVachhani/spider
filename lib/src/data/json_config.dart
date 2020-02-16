0d104ae4302832cb32c9f8b2a4f92f4a5cc00417
birjuvachhani <brvachhani@gmail.com>
2020-02-16 13:51:08 +0530

    :bug: fix create command
    Signed-off-by: birjuvachhani <brvachhani@gmail.com>

---
+++ lib/src/data/json_config.dart
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2020 Birju Vachhani
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const String JSON_CONFIGS = '''{
+  "groups": [
+    {
+      "path": "assets/images",
+      "class_name": "Assets",
+      "package": "res",
+      "types": [
+        "jpg",
+        "jpeg",
+        "png",
+        "webp",
+        "gif",
+        "bmp",
+        "wbmp"
+      ]
+    },
+    {
+      "path": "assets/vectors",
+      "class_name": "Svgs",
+      "package": "res",
+      "types": ["svg"]
+    }
+  ]
+}''';
