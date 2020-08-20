5de6a87b85030f9533cadde3790de9b118d0aafd
BirjuVachhani <brvachhani@gmail.com>
2020-08-20 15:19:11 +0530

    :green_heart: build for mac, windows and linux

--- test/spider_test.dart
+++ test/spider_test.dart
@@ -7,12 +7,6 @@ import 'package:spider/spider.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('spider test', () {
-    final spider = Spider('');
-    expect(spider.config != null, true);
-    spider.build();
-  }, timeout: Timeout(Duration(minutes: 2)));
-
   test('create config test test', () {
     Spider.createConfigs(true);
     expect(File('spider.json').existsSync(), true);
