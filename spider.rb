61596aea22ca1e7f94c660ff8bafec867a2e5c1d
Birju Vachhani <brvachhani@gmail.com>
2020-08-27 11:46:53 +0530

    Update spider.rb

--- spider.rb
+++ spider.rb
@@ -2,7 +2,7 @@ class Spider < Formula
   desc "small dart library to generate Assets dart code from assets folder."
   homepage "https://github.com/BirjuVachhani/spider"
   url "https://github.com/BirjuVachhani/spider/archive/1.0.1.tar.gz"
-  sha256 "b746721ca3809b2bdd2285a4e78ca5f7e5a692d122cf524f76f4ec330877bf7a"
+  sha256 "cb3d3af95888551ebacb76f5d7a06d196e43417d25980c84f235d5ad4f2baded"
   license "Apache-2.0"
   
   bottle :unneeded
