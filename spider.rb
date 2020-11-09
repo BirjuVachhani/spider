f35e6041ba1ec1437f5ed7338564ccb67e59010a
BirjuVachhani <brvachhani@gmail.com>
2020-11-09 11:33:40 +0530

    :bug: fix homebrew tap SHA

--- spider.rb
+++ spider.rb
@@ -2,7 +2,7 @@ class Spider < Formula
   desc "small dart library to generate Assets dart code from assets folder."
   homepage "https://github.com/BirjuVachhani/spider"
   url "https://github.com/BirjuVachhani/spider/archive/1.1.0.tar.gz"
-  sha256 "cb3d3af95888551ebacb76f5d7a06d196e43417d25980c84f235d5ad4f2baded"
+  sha256 "3f11440c57bdd64636350cdefd1b255903908b1242f7f5a7e8e5e06645d0e125"
   license "Apache-2.0"
   
   bottle :unneeded
