f3c21d02c60af4f6852d87ebb64150da85aa8007
BirjuVachhani <brvachhani@gmail.com>
2020-11-09 11:26:14 +0530

    :arrow_up: upgrade homebrew tap

--- spider.rb
+++ spider.rb
@@ -1,7 +1,7 @@
 class Spider < Formula
   desc "small dart library to generate Assets dart code from assets folder."
   homepage "https://github.com/BirjuVachhani/spider"
-  url "https://github.com/BirjuVachhani/spider/archive/1.0.1.tar.gz"
+  url "https://github.com/BirjuVachhani/spider/archive/1.1.0.tar.gz"
   sha256 "cb3d3af95888551ebacb76f5d7a06d196e43417d25980c84f235d5ad4f2baded"
   license "Apache-2.0"
   
