861bca0121d4c5ae67ac24316e99ce0e7dd29291
Alexey Zemcov <alexi.zemcov@gmail.com>
2022-01-31 17:28:38 +0300

    📝 change use_references_list default parameter

--- lib/src/data/json_config.dart
+++ lib/src/data/json_config.dart
@@ -20,7 +20,7 @@ const String JSON_CONFIGS = '''{
   "no_comments":true,
   "export":true,
   "use_part_of":true,
-  "use_references_list": true,
+  "use_references_list": false,
   "package":"resources",
   "groups": [
     {
