b464b0552a06efcb4f618a4fef8c49c7aee26f86
birjuvachhani <brvachhani@gmail.com>
2022-02-14 10:31:21 +0530

    :recycle: Format code

--- update_formula.dart
+++ update_formula.dart
@@ -2,18 +2,20 @@ import 'dart:io';
 
 // command: update_formula.dart url sha path
 void main(List<String> args) {
-  if(args.length != 3) {
+  if (args.length != 3) {
     print('Version, SHA and file path not specified.');
     exit(0);
   }
 
   final formulaFile = File(args[2]);
   String content = formulaFile.readAsStringSync();
-  // Replace URL 
-  content = content.replaceFirst(RegExp(r'url\s\".*\"'), 'url "https://github.com/BirjuVachhani/spider/archive/${args[0]}.tar.gz"');
+  // Replace URL
+  content = content.replaceFirst(RegExp(r'url\s\".*\"'),
+      'url "https://github.com/BirjuVachhani/spider/archive/${args[0]}.tar.gz"');
 
   // Replace SHA
-  content = content.replaceFirst(RegExp(r'sha256\s\".*\"'), 'sha256 "${args[1]}"');
+  content =
+      content.replaceFirst(RegExp(r'sha256\s\".*\"'), 'sha256 "${args[1]}"');
 
   formulaFile.writeAsStringSync(content);
 }
