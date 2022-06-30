beba30c4d7b6c6b10ca835e8d8f9ef74663115a9
Gleb Perveev <sanlovty@gmail.com>
2022-06-30 16:22:00 +0300

    :white_check_mark: update tests for new system

--- test/spider_test.dart
+++ test/spider_test.dart
@@ -23,18 +23,49 @@ void main() {
     "package": "resources",
     "groups": [
       {
-        "class_name": "Assets",
-        "subgroups": [
+        "class_name": "Images",
+        "path": "assets/images",
+        "types": [".png", ".jpg", ".jpeg", ".webp", ".webm", ".bmp"]
+      },
+      {
+        "class_name": "Svgs",
+        "sub_groups": [
           {
-            "path": "assets/images",
-            "types": ["jpg"],
-            "prefix": "jpg"
+            "path": "assets/svgsMenu",
+            "prefix": "menu",
+            "types": [".svg"]
           },
           {
-            "path": "assets/images",
-            "types": ["png"],
-            "prefix": "png"
+            "path": "assets/svgsOther",
+            "prefix": "other",
+            "types": [".svg"]
           }
+        ]
+      },
+      {
+        "class_name": "Ico",
+        "types": [".ico"],
+        "prefix": "ico",
+        "sub_groups": [
+          {
+            "path": "assets/icons",
+            "prefix": "test1",
+            "types": [".ttf"]
+          },
+          {
+            "path": "assets/vectors",
+            "prefix": "test2",
+            "types": [".pdf"]
+          }
+        ]
+      },
+      {
+        "class_name": "Video",
+        "types": [".mp4"],
+        "path": "assets/moviesOnly",
+        "sub_groups": [
+          {"path": "assets/movies", "prefix": "common"},
+          {"path": "assets/moviesExtra", "prefix": "extra"}
         ]
       }
     ]
@@ -70,25 +101,63 @@ void main() {
 
       spider.build();
 
-      final genFile = File(p.join('lib', 'resources', 'images.dart'));
-      expect(genFile.existsSync(), isTrue);
+      final genFile1 = File(p.join('lib', 'resources', 'images.dart'));
+      expect(genFile1.existsSync(), isTrue);
+      final genFile2 = File(p.join('lib', 'resources', 'svgs.dart'));
+      expect(genFile2.existsSync(), isTrue);
+      final genFile3 = File(p.join('lib', 'resources', 'ico.dart'));
+      expect(genFile3.existsSync(), isTrue);
+      final genFile4 = File(p.join('lib', 'resources', 'video.dart'));
+      expect(genFile4.existsSync(), isTrue);
 
       addTearDown(() {
         File(p.join('test', 'images_test.dart')).deleteSync();
       });
+      addTearDown(() {
+        File(p.join('test', 'svgs_test.dart')).deleteSync();
+      });
+      addTearDown(() {
+        File(p.join('test', 'ico_test.dart')).deleteSync();
+      });
+      addTearDown(() {
+        File(p.join('test', 'video_test.dart')).deleteSync();
+      });
 
-      final classContent = genFile.readAsStringSync();
+      final classContent1 = genFile1.readAsStringSync();
+      final classContent2 = genFile2.readAsStringSync();
+      final classContent3 = genFile3.readAsStringSync();
+      final classContent4 = genFile4.readAsStringSync();
 
-      expect(classContent, contains('class Images'));
-      expect(classContent, contains('static const String pngTest1'));
-      expect(classContent, contains('static const String jpgTest2'));
-      expect(classContent, contains('assets/images/test1.png'));
-      expect(classContent, contains('assets/images/test2.jpg'));
+      expect(classContent1, contains('class Images'));
+      expect(classContent1, contains('static const String test1'));
+      expect(classContent1, contains('static const String test2'));
+      expect(classContent1, contains('assets/images/test1.png'));
+      expect(classContent1, contains('assets/images/test2.jpg'));
+
+      expect(classContent2, contains('class Svgs'));
+      expect(classContent2, contains('static const String menuTest4'));
+      expect(classContent2, contains('static const String otherTest6'));
+      expect(classContent2, contains('assets/svgsMenu/test4.svg'));
+      expect(classContent2, contains('assets/svgsOther/test6.svg'));
+
+      expect(classContent3, contains('class Ico'));
+      expect(classContent3, contains('static const String icoTest8'));
+      expect(classContent3, contains('static const String icoTest11'));
+      expect(classContent3, contains('assets/icons/test8.ico'));
+      expect(classContent3, contains('assets/vectors/test11.ico'));
+
+      expect(classContent4, contains('class Video'));
+      expect(classContent4, contains('static const String test16'));
+      expect(classContent4, contains('assets/moviesOnly/test16.mp4'));
 
       final exportFile = File(p.join('lib', 'resources', 'resources.dart'));
-      expect(genFile.existsSync(), isTrue);
+      expect(exportFile.existsSync(), isTrue);
       final exportContent = exportFile.readAsStringSync();
+
       expect(exportContent, contains("part 'images.dart';"));
+      expect(exportContent, contains("part 'svgs.dart';"));
+      expect(exportContent, contains("part 'ico.dart';"));
+      expect(exportContent, contains("part 'video.dart';"));
     });
 
     test('asset generation test with library export on spider', () async {
@@ -101,21 +170,49 @@ void main() {
 
       spider.build();
 
-      final genFile = File(p.join('lib', 'resources', 'assets.dart'));
-      expect(genFile.existsSync(), isTrue);
+      final genFile1 = File(p.join('lib', 'resources', 'images.dart'));
+      expect(genFile1.existsSync(), isTrue);
+      final genFile2 = File(p.join('lib', 'resources', 'svgs.dart'));
+      expect(genFile2.existsSync(), isTrue);
+      final genFile3 = File(p.join('lib', 'resources', 'ico.dart'));
+      expect(genFile3.existsSync(), isTrue);
+      final genFile4 = File(p.join('lib', 'resources', 'video.dart'));
+      expect(genFile4.existsSync(), isTrue);
 
-      final classContent = genFile.readAsStringSync();
+      final classContent1 = genFile1.readAsStringSync();
+      final classContent2 = genFile2.readAsStringSync();
+      final classContent3 = genFile3.readAsStringSync();
+      final classContent4 = genFile4.readAsStringSync();
 
-      expect(classContent, contains('class Assets'));
-      expect(classContent, contains('static const String pngTest1'));
-      expect(classContent, contains('static const String jpgTest2'));
-      expect(classContent, contains('assets/images/test1.png'));
-      expect(classContent, contains('assets/images/test2.jpg'));
+      expect(classContent1, contains('class Images'));
+      expect(classContent1, contains('static const String test1'));
+      expect(classContent1, contains('static const String test2'));
+      expect(classContent1, contains('assets/images/test1.png'));
+      expect(classContent1, contains('assets/images/test2.jpg'));
+
+      expect(classContent2, contains('class Svgs'));
+      expect(classContent2, contains('static const String menuTest4'));
+      expect(classContent2, contains('static const String otherTest6'));
+      expect(classContent2, contains('assets/svgsMenu/test4.svg'));
+      expect(classContent2, contains('assets/svgsOther/test6.svg'));
+
+      expect(classContent3, contains('class Ico'));
+      expect(classContent3, contains('static const String icoTest8'));
+      expect(classContent3, contains('static const String icoTest11'));
+      expect(classContent3, contains('assets/icons/test8.ico'));
+      expect(classContent3, contains('assets/vectors/test11.ico'));
+
+      expect(classContent4, contains('class Video'));
+      expect(classContent4, contains('static const String test16'));
+      expect(classContent4, contains('assets/moviesOnly/test16.mp4'));
 
       final exportFile = File(p.join('lib', 'resources', 'resources.dart'));
-      expect(genFile.existsSync(), isTrue);
+      expect(exportFile.existsSync(), isTrue);
       final exportContent = exportFile.readAsStringSync();
-      expect(exportContent, contains("export 'assets.dart';"));
+      expect(exportContent, contains("export 'images.dart';"));
+      expect(exportContent, contains("export 'svgs.dart';"));
+      expect(exportContent, contains("export 'ico.dart';"));
+      expect(exportContent, contains("export 'video.dart';"));
     });
 
     tearDown(() {
